58774cd89fde5d55100bb64ea4e64a1ac853b21e
María Fernanda Gonzalez Lara <mariagonlar@unisabana.edu.co>
2026-04-09 19:52:26 -0500

    main 1

--- main.py
+++ main.py
@@ -2,9 +2,52 @@ from password_manager import add_login, change_password, encrypt_passwords_in_fi
 
 
 def main() -> None:
-    """TODO: Parte 5 - programa principal interactivo."""
-    pass
+    from password_manager import (
+    encrypt_passwords_in_file,
+    change_password,
+    add_login
+)
 
+def main() -> None:
+    archivo = input("Enter the CSV file name:\n")
+    encrypt_passwords_in_file(archivo)
+    while True:
+        print("Options: (1) Change Password, (2) Add Password, (3) Quit:")
+        opcion = input()
+        if opcion == 1:
+            user_input = input("Enter the website and the new password:\n")
+            partes = user_input.split()
+            if len(partes) < 2:
+                print("Input is in the wrong format!")
+                continue
+            website = partes[0]
+            password = partes[1]
+            if len(password) < 12:
+                print("Password is too short!")
+                continue
+            resultado = change_password(archivo, website, password)
+            if resultado == False:
+                print("Website not found! Operation failed.")
+            else:
+                print("Password changed.")
+        elif opcion == 2:
+            user_input = input("Enter the website, username, and password:\n")
+            partes = user_input.split()
+            if len(partes) < 3:
+                print("Input is in the wrong format!")
+                continue
+            website = partes[0]
+            username = partes[1]
+            password = partes[2]
+            if len(password) < 12:
+                print("Password is too short!")
+                continue
+            add_login(archivo, website, username, password)
+            print("Login added.")
+        elif opcion == 3:
+            break
+        else:
+            print("Invalid option selected!")
 
 if __name__ == "__main__":
     main()
